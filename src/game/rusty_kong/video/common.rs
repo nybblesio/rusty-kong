2b83a759b875f1db7fc087e5112ca656d070a8a5
Jeff Panici <jeff.panici@panici-software.com>
2018-03-27 10:00:46 -0600

    getting close to rendering

--- src/game/rusty_kong/video/common.rs
+++ src/game/rusty_kong/video/common.rs
@@ -28,11 +28,13 @@ pub const F_SPR_ENABLED: u8 = 0b00000001;
 pub const F_SPR_COLLIDED:u8 = 0b00000010;
 pub const F_SPR_HFLIP:   u8 = 0b00000100;
 pub const F_SPR_VFLIP:   u8 = 0b00001000;
+pub const F_SPR_CHANGED: u8 = 0b00010000;
 
 pub const F_BG_NONE:     u8 = 0b00000000;
 pub const F_BG_ENABLED:  u8 = 0b00000001;
 pub const F_BG_HFLIP:    u8 = 0b00000010;
 pub const F_BG_VFLIP:    u8 = 0b00000100;
+pub const F_BG_CHANGED:  u8 = 0b00001000;
 
 #[derive(Copy, Clone)]
 pub struct Palette {
@@ -47,20 +49,204 @@ pub struct PaletteEntry {
     pub a: u8
 }
 
+use sdl2::surface::SurfaceRef;
+
+#[derive(Copy, Clone)]
 pub struct SpriteControlBlock {
     y: u16,
     x: u16,
+    flags: u8,
     tile: u16,
     palette: u8,
-    flags: u8,
     user_data1: u32,
-    user_data2: u32
+    user_data2: u32,
+    //surface: SurfaceRef,
 }
 
+impl SpriteControlBlock {
+    pub fn new_control_table() -> [SpriteControlBlock; SPRITE_MAX as usize] {
+        [SpriteControlBlock::new_empty(); SPRITE_MAX as usize]
+    }
+
+    pub fn new_empty() -> SpriteControlBlock {
+        SpriteControlBlock {
+            y: 0,
+            x: 0,
+            tile: 0,
+            palette: 0,
+            flags: F_SPR_NONE,
+            user_data1: 0,
+            user_data2: 0,
+            //surface: ()
+        }
+    }
+
+    pub fn update(self:&SpriteControlBlock) {
+        use super::palettes::get_palette;
+        use super::sprites::get_sprite_bitmap;
+
+    }
+
+    pub fn tile(self:&mut SpriteControlBlock, number:u16) {
+        self.tile = number;
+    }
+
+    pub fn is_changed(self:&SpriteControlBlock) -> bool {
+        self.flags & F_SPR_CHANGED != 0
+    }
+
+    pub fn is_enabled(self:&SpriteControlBlock) -> bool {
+        self.flags & F_SPR_ENABLED != 0
+    }
+
+    pub fn is_collided(self:&SpriteControlBlock) -> bool {
+        self.flags & F_SPR_COLLIDED != 0
+    }
+
+    pub fn enable(self:&mut SpriteControlBlock, flag:bool) {
+        if flag {
+            self.flags |= F_SPR_ENABLED;
+        } else {
+            self.flags &= !F_SPR_ENABLED;
+        }
+    }
+
+    pub fn changed(self:&mut SpriteControlBlock, flag:bool) {
+        if flag {
+            self.flags |= F_SPR_CHANGED;
+        } else {
+            self.flags &= !F_SPR_CHANGED;
+        }
+    }
+
+    pub fn collided(self:&mut SpriteControlBlock, flag:bool) {
+        if flag {
+            self.flags |= F_SPR_COLLIDED;
+        } else {
+            self.flags &= !F_SPR_COLLIDED;
+        }
+    }
+
+    pub fn palette(self:&mut SpriteControlBlock, number:u8) {
+        self.palette = number;
+    }
+
+    pub fn get_position(self:&SpriteControlBlock) -> (u16, u16) {
+        (self.x, self.y)
+    }
+
+    pub fn position(self:&mut SpriteControlBlock, x:u16, y:u16) {
+        self.x = x;
+        self.y = y;
+    }
+
+    pub fn vertical_flip(self:&mut SpriteControlBlock, flag:bool) {
+        if flag {
+            self.flags |= F_SPR_VFLIP;
+        } else {
+            self.flags &= !F_SPR_VFLIP;
+        }
+    }
+
+    pub fn is_vertically_flipped(self:&SpriteControlBlock) -> bool {
+        self.flags & F_SPR_VFLIP != 0
+    }
+
+    pub fn horizontal_flip(self:&mut SpriteControlBlock, flag:bool) {
+        if flag {
+            self.flags |= F_SPR_HFLIP;
+        } else {
+            self.flags &= !F_SPR_HFLIP;
+        }
+    }
+
+    pub fn is_horizontally_flipped(self:&SpriteControlBlock) -> bool {
+        self.flags & F_SPR_HFLIP != 0
+    }
+}
+
+#[derive(Copy, Clone)]
 pub struct BackgroundControlBlock {
     tile: u16,
     flags: u8,
     palette: u8,
     user_data1: u32,
     user_data2: u32
+}
+
+impl BackgroundControlBlock {
+    pub fn new_control_table() -> [BackgroundControlBlock; (TILE_ROW_COUNT * TILE_COL_COUNT) as usize] {
+        [BackgroundControlBlock::new_empty(); (TILE_ROW_COUNT * TILE_COL_COUNT) as usize]
+    }
+
+    pub fn new_empty() -> BackgroundControlBlock {
+        BackgroundControlBlock {
+            tile: 0,
+            flags: F_BG_NONE,
+            palette: 0,
+            user_data1: 0,
+            user_data2: 0}
+    }
+
+    pub fn update(self:&BackgroundControlBlock) {
+        use super::palettes::get_palette;
+        use super::tiles::get_tile_bitmap;
+
+    }
+
+    pub fn is_changed(self:&BackgroundControlBlock) -> bool {
+        self.flags & F_BG_CHANGED != 0
+    }
+
+    pub fn is_enabled(self:&BackgroundControlBlock) -> bool {
+        self.flags & F_BG_ENABLED != 0
+    }
+
+    pub fn tile(self:&mut BackgroundControlBlock, number:u16) {
+        self.tile = number;
+    }
+
+    pub fn changed(self:&mut BackgroundControlBlock, flag:bool) {
+        if flag {
+            self.flags |= F_BG_CHANGED;
+        } else {
+            self.flags &= !F_BG_CHANGED;
+        }
+    }
+
+    pub fn palette(self:&mut BackgroundControlBlock, number:u8) {
+        self.palette = number;
+    }
+
+    pub fn enable(self:&mut BackgroundControlBlock, flag:bool) {
+        if flag {
+            self.flags |= F_BG_ENABLED;
+        } else {
+            self.flags &= !F_BG_ENABLED;
+        }
+    }
+
+    pub fn vertical_flip(self:&mut BackgroundControlBlock, flag:bool) {
+        if flag {
+            self.flags |= F_BG_VFLIP;
+        } else {
+            self.flags &= !F_BG_VFLIP;
+        }
+    }
+
+    pub fn is_vertically_flipped(self:&BackgroundControlBlock) -> bool {
+        self.flags & F_BG_VFLIP != 0
+    }
+
+    pub fn horizontal_flip(self:&mut BackgroundControlBlock, flag:bool) {
+        if flag {
+            self.flags |= F_BG_HFLIP;
+        } else {
+            self.flags &= !F_BG_HFLIP;
+        }
+    }
+
+    pub fn is_horizontally_flipped(self:&BackgroundControlBlock) -> bool {
+        self.flags & F_BG_HFLIP != 0
+    }
 }
--- src/game/rusty_kong/video/mod.rs
+++ src/game/rusty_kong/video/mod.rs
@@ -29,27 +29,42 @@ use sdl2::pixels::Color;
 use sdl2::render::WindowCanvas;
 
 lazy_static! {
-    static ref SPR_CNTL:Vec<SpriteControlBlock> = vec!();
-    static ref BG1_CNTL:Vec<BackgroundControlBlock> = vec!();
+    static ref SPR_CNTL:[SpriteControlBlock; SPRITE_MAX as usize] = SpriteControlBlock::new_control_table();
+    static ref BG1_CNTL:[BackgroundControlBlock; (TILE_ROW_COUNT * TILE_COL_COUNT) as usize] = BackgroundControlBlock::new_control_table();
 }
 
 fn video_bg(canvas: &mut WindowCanvas) {
-
+    // XXX: need Surface that is the background buffer
+    //      only changed tiles get rendered into this buffer
+    //      copy this into canvas
+    for bg_cntl in BG1_CNTL.iter() {
+        bg_cntl.update();
+    }
 }
 
+// XXX: structure passed in here should hold WindowCanvas, background buffer, and any other state
 fn video_fg(canvas: &mut WindowCanvas) {
-
+    for fg_cntl in SPR_CNTL.iter() {
+        // XXX: update internal surface
+        //      copy to canvas
+        fg_cntl.update();
+    }
 }
 
+// XXX: structure passed in here should hold WindowCanvas, background buffer, and any other state
 pub fn video_update(canvas: &mut WindowCanvas) {
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
+//    canvas.set_draw_color(Color::RGB(0, 0, 0));
+//    canvas.clear();
     video_bg(canvas);
+    //canvas.copy(background_surface, .....);
+
     video_fg(canvas);
+
     canvas.present();
 }
 
 pub fn video_init(sdl_context: &Sdl) -> WindowCanvas {
+    // XXX: set up the background buffer and package it with WindowCanvas
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem.window("Rusty Kong", SCREEN_WIDTH * 4, SCREEN_HEIGHT * 4)
         .position_centered()
